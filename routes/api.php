50c316c5f51520d4758c353e7172445b3622fb78
Yan <yan.lysechko88@mail.ru>
2026-04-26 09:30:23 +0300

    Add: basic App structure controllers / models / services

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\PriceController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderStatisticsController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/price', [PriceController::class, 'getPrice']);
+
+Route::get('/orders/statistics', [OrderStatisticsController::class, 'index']);
+
+Route::get('/orders/{id}', [OrderController::class, 'show']);
+
+Route::post('/soap', [OrderController::class, 'soapStore']);
